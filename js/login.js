40c3e5902a9975002471e27f934db142a9105f5f
rounokjahan1 <rounok.jahan29gmail.com>
2024-09-19 12:03:45 +0600

    home page added

--- js/login.js
+++ js/login.js
@@ -6,5 +6,15 @@ document.getElementById('button-login').addEventListener('click', function(event
   event.preventDefault();
   // step-3 get the phone number
   const phoneNumber = document.getElementById('phone-number').value;
-  console.log(phoneNumber)
+  const pinNumber = document.getElementById('pin-number').value;
+  console.log(phoneNumber , pinNumber);
+// validate phone and pin
+  if(phoneNumber === '5' && pinNumber === "1234"){
+    console.log('You are logged in');
+    window.location.href = '/home.html';
+
+  }
+  else {
+    alert('Wrong phone number or pin')
+  }
 })
